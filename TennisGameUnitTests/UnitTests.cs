aa8e99713f5052dedc934be12676e585cca2063a
GaetanoCorellis <gaetano.corellis@euris.it>
2018-01-17 11:58:57 +0100

    Project creation and unit tests.

---
+++ TennisGameUnitTests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameUnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void FirstTestMethod()
+        {
+        }
+    }
+}
